81b70bd3bafc7e498e17b2f7fa65854360fd6106
ROTARTSI <rotartsi0482@gmail.com>
2019-03-26 15:57:19 -0700

    yeet

--- old/JGame/docs/randColor.js
+++ old/JGame/docs/randColor.js
@@ -1,10 +1,32 @@
+function random_bg_color() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    return "rgb(" + x + "," + y + "," + z + ")";
+}
+
 let doRandColor = function () {
+    alert("Congrats on activating an easter egg! Who in their right mind would go around clicking commas on obscure sites?");
+    alert("EPILEPSY WARNING: The following easter egg contains bright, flashing lights! Be aware!");
+    myAudio = new Audio('invincible.mp3');
+    myAudio.loop = true;
+    myAudio.addEventListener('ended', function () {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+    myAudio.play();
+
+    doSubRand();
+};
+
+let doSubRand = function () {
     setInterval(function () {
         let all = document.getElementsByTagName("*");
 
         for (let i = 0; i < all.length; i++) {
             all[i].style.background = random_bg_color();
             all[i].style.color = random_bg_color();
+            all[i].style.borderColor = random_bg_color();
         }
     }, 0);
     let reload = function () {
@@ -15,4 +37,3 @@ let doRandColor = function () {
     };
     window.onunload = reload;
 };
-
