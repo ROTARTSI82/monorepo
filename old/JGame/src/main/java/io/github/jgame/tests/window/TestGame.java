d8aca0da06a9dbd20bd29699fde9573967b16098
ROTARTSI82 <38170305+rotartsi82@users.noreply.github.com>
2019-04-01 09:05:10 -0700

    Maven!
    Signed-off-by: ROTARTSI <rotartsi0482@gmail.com>

---
+++ old/JGame/src/main/java/io/github/jgame/tests/window/TestGame.java
@@ -0,0 +1,17 @@
+package io.github.jgame.tests.window;
+
+import io.github.jgame.game.Game;
+
+/**
+ * @deprecated Only use for blitting tests.
+ */
+@Deprecated
+public class TestGame extends Game {
+    public TestGame() {
+        super();
+        states.put("main", new MainState(this));
+        states.put("win_capture", new WindowCaptureState(this));
+        states.put("btTest", new ButtonTestState(this));
+        state = "main";
+    }
+}
