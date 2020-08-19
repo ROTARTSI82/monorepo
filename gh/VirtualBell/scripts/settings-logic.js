27b7a49eef40690eb56c7bff554c9380a613d09f
ROTARTSI82 <rotartsi0482@gmail.com>
2020-08-18 17:52:10 -0700

    Start work on settings overhaul

---
+++ gh/VirtualBell/scripts/settings-logic.js
@@ -0,0 +1,103 @@
+let saveSched = document.getElementById("save-sched");
+let bellSelect = document.getElementById("sound-select");
+let saveBell = document.getElementById("save-bell");
+let tbody = document.getElementById("tcust-names-bod");
+
+
+let uniqueEvents = [];
+for (let i = 0; i < 7; i++) {
+    let sched = localStorage.getItem(getDayStr(i) + "-schedule");
+    if (sched === null) {
+        sched = getDefaultSched(i);
+    }
+
+    if ( sched !== "off_duty" && sched ) {
+        let evs = sched.split("~[SE]~");
+        for (let j of evs) {
+            let uev = j.split("~[SA]~")[0];
+            if (!uniqueEvents.includes(uev) && uev !== "Break" && !uev.includes("Passing Period")) {
+                uniqueEvents.push(uev);
+            }
+        }
+    }
+}
+
+console.log(uniqueEvents);
+
+let evrHtml = "<th scope=\"row\">\n" +
+    "                    <select class=\"form-control\">\n" +
+    "                    </select>\n" +
+    "                </th>\n" +
+    "                <td>\n" +
+    "                    <input class=\"form-control\"\n" +
+    "                           placeholder=\"Custom Name [eg. 'P1 Rocket Science (Mr. Jeb Kerman)']\" type=\"text\">\n" +
+    "                </td>\n" +
+    "                <td>\n" +
+    "                    <input class=\"form-control\" id=\"per1-link\"\n" +
+    "                           placeholder=\"Links (space separated) [eg. 'https://harker.zoom.us/j/123']\"\n" +
+    "                           type=\"text\">\n" +
+    "                </td>\n" +
+    "                <td>\n" +
+    "                    <button type=\"button\" class=\"btn btn-danger\" onclick=\"this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);\">Delete</button>\n" +
+    "                </td>";
+
+let firstOption = "<option value=\"\" disabled selected>Select an Event</option>";
+
+if (localStorage.getItem("bellSound") !== null) {
+    // Uh, totally didn't ctrl v from StackOverflow
+    document.querySelector("#sound-select option[value='" + localStorage.getItem("bellSound") + "']").setAttribute('selected', true);
+}
+
+
+let rowMap = new Map();
+for (let i = 0; i < localStorage.length; i++) {
+    let k = localStorage.key(i);
+    let v = localStorage.getItem(k);
+
+    if (/^.*-link$/g.test(k)) {
+        k = k.substr(0, k.length - 5);
+        if (rowMap.has(k)) {
+            rowMap.set(k, {name: rowMap.get(k).name, link: v});
+        } else {
+            rowMap.set(k, {name: "", link: v});
+        }
+    }
+
+    if (/^.*-name$/g.test(k)) {
+        k = k.substr(0, k.length - 5);
+        if (rowMap.has(k)) {
+            rowMap.set(k, {name: v, link: rowMap.get(k).link});
+        } else {
+            rowMap.set(k, {name: v, link: ""});
+        }
+    }
+}
+
+console.log(rowMap);
+
+let previewSound = new Audio();
+saveBell.onclick = function () {
+    localStorage.setItem("bellSound", bellSelect.options[bellSelect.selectedIndex].value);
+
+    previewSound = new Audio(localStorage.getItem("bellSound"));
+    previewSound.play();
+};
+
+saveSched.onclick = function () {
+    $('#success').modal('show');
+
+};
+
+let addCustRow = function() {
+    let nr = document.createElement("tr");
+    nr.innerHTML = evrHtml;
+
+    let sel = nr.firstElementChild.firstElementChild;
+    sel.innerHTML += firstOption;
+    for (let i of uniqueEvents) {
+        sel.innerHTML += `<option value="${i}">${i}</option>`;
+    }
+
+    tbody.appendChild(nr);
+}
+
