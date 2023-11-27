b7e3023c4d9cd7222b5499db3512b09fad184b3d
Rotartsi <rotartsi0482@gmail.com>
2023-11-27 12:41:43 -0800

    IT FUCKING WORKED WTF

--- gh/ATCS-ATNeural/chessnn/audiogen.py
+++ gh/ATCS-ATNeural/chessnn/audiogen.py
@@ -4,17 +4,25 @@ model = model.eval()
 
 
 def gen():
-    start = random.randint(0, list(data.shape)[0] - 1026)
+    start = 4096 # 4096 - 250 # random.randint(0, list(data.shape)[0] - 1026)
     generation = data[start:start+64].tolist()
     ctx = 64
     while ctx < 1024:
-        gen_tensor = torch.tensor(generation, dtype=dtype, device=device)
-        new = model(gen_tensor + pos_enc[:ctx], ctx)
+        gen_tensor = torch.tensor(generation[-1024:], dtype=dtype, device=device)
+        new = model(gen_tensor, ctx)
         ctx += 1
-        print(new.shape)
+        print(len(generation))
         # print(new)
         generation.append(new[-1].tolist())
-    return np.array(generation, dtype=np.float32)
+    return np.array(generation[-1024:], dtype=np.float32)
+
+
+def preserve_test():
+    start = random.randint(0, list(data.shape)[0] - 1026)
+    buf = torch.tensor(data[start:start+1024], dtype=dtype, device=device)
+    for i in range(1):
+        buf = model(buf, 1024)
+    return buf.numpy(force=True)
 
 
 generation = gen()
--- gh/ATCS-ATNeural/chessnn/audiogen_train.py
+++ gh/ATCS-ATNeural/chessnn/audiogen_train.py
@@ -22,30 +22,32 @@ SR = 16000
 
 pos_enc = [[(math.sin if i%2 == 0 else math.cos)(pos * math.pi / 2**(10 * (i//2)/256)) for i in range(512)]
            for pos in range(1024)]
-pos_enc = 2*torch.tensor(pos_enc, dtype=dtype, device=device)
+pos_enc = torch.tensor(pos_enc, dtype=dtype, device=device)
 
 
 class AudioGen(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.trans = torch.nn.Transformer(512, num_encoder_layers=6, batch_first=True,
-                                          dim_feedforward=2048, num_decoder_layers=6, activation=torch.nn.GELU(),
+        self.lin1 = torch.nn.Sequential(torch.nn.Linear(512, 512), torch.nn.GELU())
+        self.trans = torch.nn.Transformer(512, num_encoder_layers=1, batch_first=True,
+                                          dim_feedforward=2048, num_decoder_layers=4, activation=torch.nn.GELU(),
                                           norm_first=True, bias=True, dropout=0.1, dtype=dtype)
         # add convolution layer? how would you handle it being a mixture of real/imag parts?
         # probably group=2, but then i'd need to separate it into 2 channels which is a pain!
 
     def forward(self, audio, ctxlen):
+        t_in = self.lin1(audio) + pos_enc[:ctxlen]
         mask = torch.nn.Transformer.generate_square_subsequent_mask(ctxlen, device=device, dtype=dtype)
-        trans = self.trans(audio, audio, src_mask=mask, tgt_mask=mask, memory_mask=mask,
+        trans = self.trans(t_in, t_in, src_mask=mask, tgt_mask=mask, memory_mask=mask,
                            src_is_causal=True, tgt_is_causal=True, memory_is_causal=True)
         return trans
 
 
 model = AudioGen().to(device).train(True)
-opt = torch.optim.AdamW(model.parameters(), lr=1.5e-4, eps=1e-5, betas=(0.9, 0.95), weight_decay=0.1)
-loss_fn = torch.nn.SmoothL1Loss()
+opt = torch.optim.AdamW(model.parameters(), lr=3e-5, eps=1e-5, betas=(0.9, 0.95), weight_decay=0.1)
+loss_fn = torch.nn.MSELoss()
 
-model.load_state_dict(torch.load("audiogen_big.txt"), strict=False)
+model.load_state_dict(torch.load("audiogen_big.txt"))
 
 fp = read("input.wav")  # MUST be SR = 16kHz
 wav = np.array(fp[1], dtype=npdtype)
@@ -54,9 +56,12 @@ stft = stft.transpose()  # by default, dimensions are (freq, time)
 print(stft.shape, stft.dtype)
 print(stft)
 data = stft.view(np.float64).reshape((list(stft.shape)[0], 512))
-data /= 1e-4 + np.abs(data).max(1, keepdims=True)
+print(f"mean {np.mean(data.flatten())} dev {np.std(data.flatten())} abs_sigma = {np.abs(data).flatten().std()}")
+data /= 1e-4 + 2.1*np.abs(data).flatten().std()  # THIS IS THE MAGIC LINE THAT MAKES IT WORK!!
 print(data.shape, data.dtype)
 print(f"fourier shape {stft.shape}")
+
+print(f"mean {np.mean(data.flatten())} dev {np.std(data.flatten())} 2sigma = {2.1*np.abs(data).flatten().std()}")
 
 
 def write_out(net_repr, name="out.wav"):
@@ -73,6 +78,7 @@ def write_out(net_repr, name="out.wav"):
 
 write_out(data[4096:4096+1024], "data.wav")
 
+print(f"parameters: {sum(p.numel() for p in model.parameters())}")
 if __name__ == "__main__":
     for batch in range(chunk):
         opt.zero_grad()
@@ -80,11 +86,11 @@ if __name__ == "__main__":
         expected_out = []
         for i in range(4):
             #
-            start = 4096*i if random.randint(0, 100) <= 92 else random.randint(0, list(data.shape)[0] - 1026)
+            start = 4096*i if random.randint(0, 100) <= 75 else random.randint(0, list(data.shape)[0] - 1026)
             audio.append(data[start:start+1024])
-            expected_out.append(data[start:start+1024]) # todo: change back to offset
+            expected_out.append(data[start+1:start+1025])  # todo: change back to offset
         audio_tensor = torch.tensor(np.array(audio), dtype=dtype, device=device)
-        audio_out = model(audio_tensor + pos_enc, 1024)
+        audio_out = model(audio_tensor, 1024)
         expected = torch.tensor(np.array(expected_out), dtype=dtype, device=device)
         # print(audio_out, '\n', expected_out)
         loss = loss_fn(audio_out, expected)
