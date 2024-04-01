8f7ce598c3dbe5311ad941e26aa2ac71d170b503
Grant Yang <rotartsi0482@gmail.com>
2024-04-01 14:51:46 -0700

    javadoc ast

--- src/ast/Expression.java
+++ src/ast/Expression.java
@@ -4,12 +4,31 @@ import parser.BoxedValue;
 
 import static ast.NamedExpression.namedOp;
 import static parser.BoxedValue.box;
-import static parser.BoxedValue.newNamed;
 
+/**
+ * Expression.java
+ * @author Grant Yang
+ * @version 2024.03.28
+ * An expression which can be evaluated to a BoxedValue
+ */
 public interface Expression
 {
+    /**
+     * Evaluates the expression to its value.
+     * @param env The environment to evaluate in
+     * @return The value of the expression
+     * @postcondition The environment may be modified by the expression
+     *                if the expression contained any := operators.
+     */
     BoxedValue eval(Environment env);
 
+    /**
+     * The constant boolean literal for TRUE
+     */
     Expression TRUE = namedOp((e) -> box(true), "true");
+
+    /**
+     * The constant boolean literal for FALSE
+     */
     Expression FALSE = namedOp((e) -> box(false), "false");
 }
--- src/ast/ForLoop.java
+++ src/ast/ForLoop.java
@@ -2,11 +2,26 @@ package ast;
 
 import parser.BoxedValue;
 
+/**
+ * ForLoop.java
+ * @author Grant Yang
+ * @version 2024.03.28
+ * A for loop, which executes a statement a certain number of times.
+ * The loop takes the loop variable from its starting value to
+ * one less than the stopping value (it is exclusive).
+ * For example, `FOR i := 0 TO 10 DO` will execute the body 10 times.
+ */
 public class ForLoop implements Statement
 {
     private final Expression start, stop;
     private final Statement body;
 
+    /**
+     * Constructs a new for loop
+     * @param start The starting value of the loop variable
+     * @param stop The stopping value of the loop variable (exclusive).
+     * @param body The code to execute
+     */
     public ForLoop(Expression start, Expression stop, Statement body)
     {
         this.start = start;
@@ -14,8 +29,15 @@ public class ForLoop implements Statement
         this.body = body;
     }
 
+    /**
+     * Execute the for loop, calling exec() on the body until the stopping condition is met.
+     * @param env The environment to execute in
+     * @postcondition `start` and `stop` have each been evaluated once,
+     *                and `body` has been executed until either a BREAK was reached
+     *                or the loop variable reached the value of `stop`.
+     */
     @Override
-    public void exec(Environment env) throws ContinueException, BreakException
+    public void exec(Environment env)
     {
         BoxedValue val = start.eval(env);
         BoxedValue max = stop.eval(env);
