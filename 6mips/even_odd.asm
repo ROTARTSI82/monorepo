20276503f308ea1eaf73b787f1d1acafdb3a2d44
Grant Yang <rotartsi0482@gmail.com>
2024-04-22 10:47:04 -0700

    mips more

--- 6mips/even_odd.asm
+++ 6mips/even_odd.asm
@@ -5,8 +5,9 @@
 # Assembly program to check if the inputted integer is even or odd.
 
 .data
-msg: .ascii "Even\0Odd\0"
+msg: .ascii "Even\n\0Odd\n\0"
 prompt: .asciiz "Enter Number: "
+done: .asciiz "Done!\n"
 
 .text 0x00400000
 .globl main
@@ -15,16 +16,25 @@ main:
 	la $a0 prompt
 	syscall
 	
-	li $t2 5
-	move $v0 $t2
+	li $v0 5
 	syscall
+	beqz $v0 exit
 	
 	andi $t0 $v0 1
 	move $t1 $0
+	
+	li $t2 6
 	movn $t1 $t2 $t0
 	
 	li $v0 4
 	la $a0 msg($t1)
+	syscall
+	
+	j main
+	
+exit:
+	li $v0 4
+	la $a0 done
 	syscall
 	
 	li $v0 10
