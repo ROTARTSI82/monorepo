c4479e0600c7d8613503fecbb7dfd79dd236e7af
AtharvGoel <AtharvGoel0@gmail.com>
2024-11-03 15:24:41 -0800

    cursed haskell but also better naming

--- gh/InfoTheoryGames/app/Main.hs
+++ gh/InfoTheoryGames/app/Main.hs
@@ -1,6 +1,9 @@
 import Text.Regex.Posix
 import Data.List
 
+(.:) :: (b -> c) -> (a1 -> a2 -> b) -> a1 -> a2 -> c
+(.:) = (.) . (.)
+
 matches :: String -> [String] -> [Bool]
 matches = map . flip (=~)
 
@@ -8,7 +11,7 @@ count :: [Bool] -> Double
 count = fromIntegral . length . filter id
 
 matchesCount :: String -> [String] -> Double
-matchesCount = ((.) . (.)) count matches
+matchesCount = count .: matches
 
 combs :: String -> Char -> [String]
 combs [] y = []
@@ -20,34 +23,33 @@ combs (x:xs) y =
     else map (x:) (combs xs y)
 
 pos :: [String] -> [String] -> [Double]
-pos x y = map (\i -> matchesCount i y) x
+pos wordList = map (\word -> matchesCount word wordList)
 
 entropy :: String -> Char -> [String] -> Double
-entropy x y z = 
+entropy currentWord charGuess wordList = 
     negate . sum $ filter (not . isNaN) (zipWith (*) p (map (logBase 2) p))
-    where p = map (/ matchesCount x z) (pos (combs x y) z)
+    where p = map (/ matchesCount currentWord wordList) (pos wordList (combs currentWord charGuess))
 
 entropyMax :: String -> String -> [String] -> Maybe Char
-entropyMax x y z = 
-    if maximum e == 0 then Nothing
-    else case elemIndex (maximum e) e of
-        Just n -> Just ((['a'..'z'] \\ y) !! n)
+entropyMax currentWord guessedChars wordList = 
+    if maximum h == 0 then Nothing
+    else case elemIndex (maximum h) h of
+        Just i -> Just ((['a'..'z'] \\ guessedChars) !! i)
         Nothing -> error "Not Found?"
-    where e = [entropy x i z | i <- ['a'..'z'] \\ y]
+    where h = [entropy currentWord charGuess wordList | charGuess <- ['a'..'z'] \\ guessedChars]
 
-cut :: String -> Char -> [String] -> [String]
-cut x y z = 
-    filter (\w -> w =~ p) z
-    where p = concat $ map (\c -> if c == '.' then "[^" ++ [y] ++ "]" else [c]) x
+cut :: Char -> [String] -> String -> [String]
+cut charGuess wordList currentWord = 
+    filter (flip (=~) reg) wordList
+    where reg = concat $ map (\c -> if c == '.' then "[^" ++ [charGuess] ++ "]" else [c]) currentWord
     
 ask :: String -> String -> [String] -> IO ()
-ask x y z = 
-    case entropyMax x y z of
-        Nothing -> putStrLn ("Solved: " ++ (filter (\w -> w =~ x) z) !! 0)
-        Just c -> do
-            putStrLn ([c] ++ "?")
-            w <- getLine
-            ask w ([c]++y) (cut w c z)
+ask currentWord guessedChars wordList = 
+    case entropyMax currentWord guessedChars wordList of
+        Nothing -> putStrLn ("Solved: " ++ (filter (flip (=~) currentWord) wordList) !! 0)
+        Just charGuess -> do
+            putStrLn $ (:) charGuess "?"
+            getLine >>= flip ask (charGuess:guessedChars) <*> cut charGuess wordList
 
 main :: IO ()
 main = do
