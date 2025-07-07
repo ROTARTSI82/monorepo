40c6b2274fb05bdfab42eab2dc512fe6d4fb2e86
Grant Yang <rotartsi0482@gmail.com>
2025-07-06 21:45:04 -0700

    idk c++ coro

---
+++ undergrad/mcserve/cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "async.cpp"
+
+int main() {
+    next = new std::queue<std::coroutine_handle<>>;
+    auto fut = lol();
+    next->push(fut.frame);
+    while (!next->empty()) {
+        std::cout << "\t task list = " << next->size() << " resuming " << (uint64_t) next->front().address() << '\n';
+        next->front().resume();
+        next->pop();
+    }
+
+    std::cout << "finished!\n";
+    delete next;
+    return 0;
+}
