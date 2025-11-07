83b888deae4934c22fc7d92d4dfb702ac3065c0c
Grant Yang <rotartsi0482@gmail.com>
2025-11-07 11:56:34 -0800

    p2 start

--- undergrad/cse123/src/p2/P2Testing.java
+++ undergrad/cse123/src/p2/P2Testing.java
@@ -1,26 +1,51 @@
 import org.junit.jupiter.api.*;
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class P2Testing {
 
+    private List<Region> scenario;
+    private double totalCost;
+    private static final Random RAND = new Random();
+
+    public void setUpRandom() {
+        scenario = P2Client.createRandomScenario(16, 10, 100, 10, 100);
+        totalCost = 0;
+        for (Region region : scenario)
+            totalCost += region.getCost();
+    }
+
     @Test
     @DisplayName("Aid All")
     public void firstTestCase() {
-        // TODO: Implement this test
-        assertEquals(false, true);
+        setUpRandom();
+        double nudge = RAND.nextDouble(10);
+        Allocation alloc = P2Client.allocateRelief(totalCost + nudge, scenario);
+
+        Allocation expected = new Allocation();
+        for (Region region : scenario)
+            expected = expected.withRegion(region);
+        assertEquals(expected, alloc);
     }
 
     @Test
     @DisplayName("Aid None")
     public void secondTestCase() {
-        // TODO: Implement this test
-        assertEquals(false, true);
+        setUpRandom();
+        Allocation alloc = P2Client.allocateRelief(RAND.nextDouble(10), scenario);
+        assertEquals(new Allocation(), alloc);
     }
 
     @Test
     @DisplayName("Aid Some")
     public void thirdTestCase() {
-        // TODO: Implement this test
-        assertEquals(false, true);
+        scenario = P2Client.createSimpleScenario();
+        Allocation alloc = P2Client.allocateRelief(2500, scenario);
+
+        Allocation expected = new Allocation();
+        expected = expected.withRegion(scenario.getLast());
+        expected = expected.withRegion(scenario.get(1));
+        expected = expected.withRegion(scenario.getFirst());
+        assertEquals(expected, alloc);
     }
 }
