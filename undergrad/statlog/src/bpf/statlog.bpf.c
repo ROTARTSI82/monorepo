f00a6ef2701641d66e7c0c466889fdca1d2ed15b
Grant Yang <rotartsi0482@gmail.com>
2026-05-31 17:08:05 -0700

    fix binsize

--- undergrad/statlog/src/bpf/statlog.bpf.c
+++ undergrad/statlog/src/bpf/statlog.bpf.c
@@ -41,9 +41,28 @@ static __always_inline void increment_map(void *map, u64 val)
 SEC("tp_btf/block_rq_complete")
 int BPF_PROG(block_rq_complete, struct request *rq, int error, unsigned int nr_bytes)
 {
-	if ((rq->cmd_flags & 255) == REQ_OP_WRITE)
+	if (error != 0) return 0;
+
+	struct request_queue *q = rq->q;
+	if (!q || !(q->disk)) return 0;
+
+	// // Skip virtual devices to avoid double-counting (e.g. counting both LV and PV).
+	// // Physical disks typically start with sd, nvme, vd, hd.
+	// // We specifically exclude dm- (LVM/LUKS), loop, and md (RAID).
+	// char c0 = q->disk->disk_name[0];
+	// char c1 = q->disk->disk_name[1];
+	// char c2 = q->disk->disk_name[2];
+
+	// if (c0 == 'd' && c1 == 'm' && c2 == '-') return 0; // dm-X
+	// if (c0 == 'l' && c1 == 'o' && c2 == 'o') return 0; // loopX
+	// if (c0 == 'm' && c1 == 'd') {
+	//    if (c2 >= '0' && c2 <= '9') return 0; // mdX
+	// }
+
+	blk_opf_t op = rq->cmd_flags & 255;
+	if (op == REQ_OP_WRITE) // || op == REQ_OP_WRITE_ZEROES || op == REQ_OP_ZONE_APPEND)
 		increment_map(&disk_writes, nr_bytes);
-	else if ((rq->cmd_flags & 255) == REQ_OP_READ)
+	else if (op == REQ_OP_READ)
 		increment_map(&disk_reads, nr_bytes);
 	return 0;
 }
@@ -51,6 +70,12 @@ int BPF_PROG(block_rq_complete, struct request *rq, int error, unsigned int nr_b
 SEC("tp_btf/netif_receive_skb")
 int BPF_PROG(netif_receive_skb, struct sk_buff *skb)
 {
+	struct net_device *dev = skb->dev;
+	if (dev->flags & IFF_LOOPBACK)
+		return 0;
+	if (dev->rtnl_link_ops)
+		return 0;
+
 	increment_map(&net_rx, skb->len);
 	return 0;
 }
@@ -58,6 +83,12 @@ int BPF_PROG(netif_receive_skb, struct sk_buff *skb)
 SEC("tp_btf/net_dev_queue")
 int BPF_PROG(net_dev_queue, struct sk_buff *skb)
 {
+	struct net_device *dev = skb->dev;
+	if (dev->flags & IFF_LOOPBACK)
+		return 0;
+	if (dev->rtnl_link_ops)
+		return 0;
+
 	increment_map(&net_tx, skb->len);
 	return 0;
 }
