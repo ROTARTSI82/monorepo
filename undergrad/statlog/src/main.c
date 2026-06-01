bfd673cc31fc5e176da4eecacecabdbf044a6b65
Grant Yang <rotartsi0482@gmail.com>
2026-05-31 23:45:23 -0700

    cli nice

--- undergrad/statlog/src/main.c
+++ undergrad/statlog/src/main.c
@@ -2,6 +2,7 @@
 #include <signal.h>
 #include <time.h>
 #include <bpf/libbpf.h>
+#include <sys/timerfd.h>
 
 #include "statread.c"
 
@@ -107,18 +108,47 @@ int try_start_bpf(int bpfout[2], int bpftrig[2]) {
   return pid;
 }
 
-int main(int argc, char **_) {
+int main(int argc, char **argv) {
   signal(SIGPIPE, SIG_IGN);
 
-  bool csvmode = argc > 1;
+  bool csvmode = false, noheader = false;
+  long poll_interval_ms = 1000;
+  char **arg = argv + 1;
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(*arg, "--csv") == 0 || strcmp(*arg, "-c") == 0)
+      csvmode |= true;
+    else if (strcmp(*arg, "--no-header") == 0 || strcmp(*arg, "-n") == 0)
+      noheader = true;
+    else if (strstr(*arg, "help") || strcmp(*arg, "-h") == 0) {
+    help:
+      printf("statlog 2026.05.31 - a simple utility to log hardware usage over time\n"
+             "Author: Grant Yang - rotartsi0482@gmail.com\n"
+             "\t-c, --csv\t\t\toutput as a csv instead of a human-readable table\n"
+             "\t-i, --interval=INT\t\tset the repeat interval in milliseconds\n"
+             "\t-n, --no-header\t\t\tdo not print the csv header\n"
+             "\t-h, --help\t\t\tprint this help message\n");
+      return !running;
+    } else if (strcmp(*arg, "--interval") == 0 || strcmp(*arg, "-i") == 0) {
+      if (i++ >= argc - 1) goto unrecog;
+      poll_interval_ms = atol(*++arg);
+    } else {
+    unrecog:
+      printf("unrecognized or malformed option %s\n\n", *arg);
+      running = 0;
+      goto help;
+    }
+    arg++;
+    continue;
+  }
+
   int bpfout[2] = {-1, -1};
   int bpftrig[2] = {-1, -1};
   int pid = getuid() == 0 ? try_start_bpf(bpfout, bpftrig) : -1;
   if (pid == -2)
     return 0;
   
-  printf("time,cpu,proc,mem,rpm,pwr,mintemp,maxtemp,tx,rx,diskr,diskw\n");
-  // printf("uid %d gid %d\n", getuid(), getgid());
+  if (csvmode && !noheader)
+    printf("time,cpu,proc,mem,rpm,pwr,mintemp,maxtemp,tx,rx,diskr,diskw\n");
 
   {
     struct sigaction config = {{exit_handler}, {0}, 0, NULL};
@@ -129,11 +159,26 @@ int main(int argc, char **_) {
   struct differential_t integral = {0};
   struct stats_t stats = {0};
   
+  int timerfd = timerfd_create(CLOCK_MONOTONIC, 0);
+  {
+    struct itimerspec utmr = {
+      .it_interval = {
+        .tv_sec = poll_interval_ms / 1000,
+        .tv_nsec = (poll_interval_ms % 1000) * 1000000},
+      .it_value = {.tv_sec = 0, .tv_nsec = 1}
+    };
+    if (timerfd == -1 || timerfd_settime(timerfd, 0, &utmr, NULL) == -1) {
+      printf("cannot set timer: %s\n", strerror(errno));
+      running = false;
+    }
+  }
+  
   while (running) {
-    struct timespec ts = {0, 0};
+    struct timespec ts;
+    // delay, reuse ts as a buffer (nothing should be written)
+    read(timerfd, &ts, sizeof(ts));
     clock_gettime(CLOCK_REALTIME, &ts);
     double unix_time = ts.tv_sec + ts.tv_nsec / 1e9;
-
 
     int result = read_stats(&stats, &integral);
     if (result != 0) {
@@ -143,11 +188,11 @@ int main(int argc, char **_) {
     
     double mem_frac = 1 - stats.mem_avail / (double) stats.mem_total;
 
-    printf(csvmode ? "%f," : "\tt %f", unix_time);
-    printf(csvmode ? "%f," : "\tcpu %f", stats.cpu_tot_util);
+    printf(csvmode ? "%.3f," : "\tt %.3f", unix_time);
+    printf(csvmode ? "%.2f," : "\tcpu %.2f", stats.cpu_tot_util);
     printf(csvmode ? "%ld," : "\tdproc %ld", stats.newproc_diff);
-    printf(csvmode ? "%f," : "\tmem %f\n", mem_frac);
-    printf(csvmode ? "%d,%f,%f,%f," : "\trpm %d\twatts %f\ttemps %f %f\n",
+    printf(csvmode ? "%.3f," : "\tmem %.3f\n", mem_frac);
+    printf(csvmode ? "%d,%.2f,%.2f,%.2f," : "\trpm %d\twatts %.2f\ttemps %.2f %.2f\n",
            stats.fan_rpm, stats.wattage, stats.min_temp, stats.max_temp);
     
     // ipc with the root bpf process
@@ -163,7 +208,7 @@ int main(int argc, char **_) {
       stats.io_diff.diskr = new.diskr - integral.io_last.diskr;
       stats.io_diff.diskw = new.diskw - integral.io_last.diskw;
     
-      printf(csvmode ? "%llu,%llu,%llu,%llu," : "\ttx %llu\trx %llu\tr %llu\tw %llu\n",
+      printf(csvmode ? "%llu,%llu,%llu,%llu" : "\ttx %llu\trx %llu\tr %llu\tw %llu\n",
              stats.io_diff.nettx, stats.io_diff.netrx, stats.io_diff.diskr, stats.io_diff.diskw);
 
       integral.io_last = new;
@@ -172,10 +217,9 @@ int main(int argc, char **_) {
   cont:
     // exit if printing fails i guess.
     if (printf("\n") < 0 || fflush(stdout) < 0) break;
-    if (!running) break;
-    sleep(3);
   }
 
+  close(timerfd);
   printf("goodbye!\n");
   if (pid > 0) {
     int status = 0;
