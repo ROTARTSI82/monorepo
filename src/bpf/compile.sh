ab68acc5531ebcb1f1b7edd3be445cf2860195c2
Grant Yang <rotartsi0482@gmail.com>
2026-05-31 17:08:05 -0700

    fix binsize

--- src/bpf/compile.sh
+++ src/bpf/compile.sh
@@ -1,3 +1,10 @@
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
-clang -g -O3 -target bpf -D__TARGET_ARCH_x86 -c statlog.bpf.c -o statlog.bpf.o
+clang -g -Os \
+      -target bpf -D__TARGET_ARCH_x86 \
+      -mcpu=probe \
+      -fno-asynchronous-unwind-tables \
+      -fmerge-all-constants \
+      -Wno-missing-declarations \
+      -c statlog.bpf.c -o statlog.bpf.o
+llvm-strip -g statlog.bpf.o
 bpftool gen skeleton statlog.bpf.o > statlog.skel.h
